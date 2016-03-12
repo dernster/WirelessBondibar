44af2d7223974d929c73def1bbf39b0665749056
Alvaro Larrosa <alarrosa14@gmail.com>
2016-03-11 23:56:52 -0300

    Without arrived_count

--- TimeClock.h
+++ TimeClock.h
@@ -2,28 +2,21 @@
 #include "utils.h"
 #include "Arduino.h"
 
-#define MAX_LONG (2147483647)
-
-#define EXPIRATION_PERIOD (1*60*1000)
-#define ARRIVED_COUNT (3)
+#define EXPIRATION_PERIOD (3*60*1000)
 
 class TimeClock{
 SINGLETON_H(TimeClock)
 public:
   void setup(){
-    minimumOffset = MAX_LONG;
+    minimumOffset = 2147483647;
     multiplier = 1;
     increment = 0;
     firstTime = true;
     correction = 0;
-    arrivedCount = 0;
-    maxArrivedOffset = 0;
   }
 
   long correction;
-  int arrivedCount = 0;
-  long maxArrivedOffset = MAX_LONG;
-  unsigned long minimumOffset = MAX_LONG;
+  unsigned long minimumOffset = 2147483647;
   unsigned long multiplier = 1;
   int increment = 0;
   bool firstTime = true;
@@ -38,7 +31,7 @@ public:
         count = 1;
         Serial.printf("Expiration needed at: %lu\n", currentTime);
       }
-      minimumOffset = MAX_LONG;
+      minimumOffset = 2147483647;
     } else {
       count = 0;
     }
@@ -46,36 +39,21 @@ public:
     if (!firstTime) {
       if (currentTime >= multiplier*EXPIRATION_PERIOD) {
         multiplier++;
-        arrivedCount = 0;
-        minimumOffset = expirationNeeded ? MAX_LONG : ((unsigned long)(abs(correction) + pow(2,increment)));
+        minimumOffset = expirationNeeded ? 2147483647 : ((unsigned long)(abs(correction) + pow(2,increment)));
         Serial.printf("Expiration -> minOff %i\n", minimumOffset);
         increment++;
       }
     } else {
-      arrivedCount = 0;
-      maxArrivedOffset = 0;
       correction = serverOffset;
       firstTime = false;
       multiplier = ((double)time()/(double)EXPIRATION_PERIOD) + 1;
     }
 
     if (((unsigned long)abs(serverOffset)) < minimumOffset){
-      arrivedCount++;
-
-      if (((unsigned long)abs(serverOffset)) > ((unsigned long)abs(maxArrivedOffset))) {
-          maxArrivedOffset = serverOffset;
-      }
-
-      Serial.printf("Found %i offset\n", arrivedCount);
-      if (arrivedCount >= ARRIVED_COUNT) {
-        Serial.printf("--- Now synchronizing ---\n");
-        // Serial.printf("Setting offset %i\n", serverOffset);
-        minimumOffset = (unsigned long)abs(maxArrivedOffset);
-        correction = maxArrivedOffset;
-        increment = 0;
-        arrivedCount = 0;
-        maxArrivedOffset = 0;
-      }
+      // Serial.printf("Setting offset %i\n", serverOffset);
+      minimumOffset = (unsigned long)abs(serverOffset);
+      correction = serverOffset;
+      increment = 0;
     }
 
     // Serial.printf("serverOffset=%i\tminimumOffset=%i\tcorrection=%i\tmultiplier=%lu\ttime=%lu\n================\n",serverOffset,minimumOffset, correction, multiplier, time());
